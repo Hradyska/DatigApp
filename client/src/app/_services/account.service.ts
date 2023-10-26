c538d492c22efa1c34ff7a8024ceeefb512f460f
Larysa Hradyska <64253822+Hradyska@users.noreply.github.com>
2023-10-26 13:28:23 +0300

    User Interface

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,26 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../_models/user';
-import { map } from 'rxjs/internal/operators/map';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   private currentUserSource = new BehaviorSubject<User | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
 
 
   constructor(private http: HttpClient) { }
-  
-  login(model: any)
-  {
+
+  login(model: any) {
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
       map((response: User) => {
         const user = response;
-        if(user){
+        if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
         }
@@ -43,7 +42,7 @@ export class AccountService {
     this.currentUserSource.next(user);
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
   }
