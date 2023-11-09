7cda032772b650b865577f8e8337cff7b7ed3d4b
Larysa Hradyska <64253822+Hradyska@users.noreply.github.com>
2023-11-09 18:52:48 +0200

    adding Likes feature

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,7 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using API.Extensions;
-
-namespace API.Entities;
+﻿namespace API.Entities;
 
 public class AppUser
 {
@@ -20,8 +17,7 @@ public class AppUser
     public string City { get; set; }
     public string Country { get; set; }
     public List<Photo> Photos { get; set; } = new();
-    // public int GetAge()
-    // {
-    //     return DateOfBirth.CalculateAge();
-    // }
+    public List<UserLike> LikedByUsers { get; set; }
+    public List<UserLike> LikedUsers { get; set; }
+
 }
---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,11 @@
+﻿using API.Entities;
+using API.Helpers;
+
+namespace API.Interfaces;
+
+public interface ILikesRepository
+{
+    Task<UserLike> GetUserLike(int soureUserId, int targetUserId);
+    Task<AppUser> GetUserWithLikes(int userId);
+    Task<PagedList<LikeDto>> GetUserLikes(LikesParams likeParams);
+}
